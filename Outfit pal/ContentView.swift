cad1aa4601f45af1484078c7a1725f89adaeb280
trissaik <trissaikyt.g@gmail.com>
2022-08-12 01:55:28 +0200

    Initial Commit

---
+++ Outfit pal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Outfit pal
+//
+//  Created by Oscar Gonzalez Strozzi on 12/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Outfit pal/Outfit_palApp.swift
@@ -0,0 +1,17 @@
+//
+//  Outfit_palApp.swift
+//  Outfit pal
+//
+//  Created by Oscar Gonzalez Strozzi on 12/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Outfit_palApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
